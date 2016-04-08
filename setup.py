8cfd594e0e60abbd1667daa2b39225b0f1012a46
dberube4 <dberube98@gmail.com>
2016-04-08 07:51:50 -0400

    setup.py update

--- setup.py
+++ setup.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from sys import argv
+
+import glob
+from os.path import basename
+
+extensions = []
+if not '--no-extensions' in argv:
+    for ext in glob.glob('pyshaders_extensions/*.py'):
+        if 'create_mmo' in ext or '__init__' in ext:
+            continue
+
+        name = basename(ext).split('.')[0]
+        extensions.append('pyshaders_extensions.'+name)        
+
+else:
+    argv.remove('--no-extensions')
+   
 
 setup(name='pyshaders',
       version='1.1.0',
@@ -9,5 +26,5 @@ setup(name='pyshaders',
       author_email='gdube@azanka.ca',
       license='MIT',
       url= 'https://github.com/gabdube/pyshaders',
-      py_modules=('pyshaders',)
+      py_modules=['pyshaders']+extensions,
      )
