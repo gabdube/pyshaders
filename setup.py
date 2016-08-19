3fc01e4a3da00efd07e3afccf4e73d28377b7312
Gabriel Dube <gdube.475@gmail.com>
2016-08-19 12:52:58 -0400

    version bump

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ else:
    
 
 setup(name='pyshaders',
-      version='1.4.0',
+      version='1.4.1',
       description='OpenGL shader wrapper for python',
       author='Gabriel Dubé',
       author_email='gdube@azanka.ca',
