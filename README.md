836e23b3d2641a70a9dc4e9c257645e43d95433d
George Zhang <geetransit@gmail.com>
2020-09-07 17:23:01 -0400

    Add copyright

--- README.md
+++ README.md
@@ -79,6 +79,7 @@ License
 >MIT License
 
 >Copyright (c) 2016 Gabriel Dubé
+>Copyright (c) 2020 George Zhang
 
 >Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- pyshaders.py
+++ pyshaders.py
@@ -2,7 +2,8 @@
 """
 ''MIT License
 
-Copyright (c) 2016 Gabriel Dubé, George Zhang
+Copyright (c) 2016 Gabriel Dubé
+Copyright (c) 2020 George Zhang
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
